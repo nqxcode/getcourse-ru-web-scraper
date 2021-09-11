4510e58a439730c8f889b8ce38e3b5158fcdafe4
Andrey Pokoev <nc101ux@gmail.com>
2021-09-11 15:50:10 +0300

    Добавлен скрипт для выкачивания anatomystudy.ru

---
+++ tools/download_site.sh
@@ -0,0 +1,15 @@
+wget \
+--recursive \
+--convert-links \
+--level=inf \
+--page-requisites \
+--adjust-extension \
+--no-clobber \
+--restrict-file-names=windows \
+--span-hosts \
+--domains anatomystudy.ru,getcourse.ru \
+--no-host-directories \
+--header='Cookie: PHPSESSID5=9a7b741b18e7c40a7a6eaab053539f1f;' \
+--execute robots=off \
+\
+https://anatomystudy.ru/teach/control/stream/index
