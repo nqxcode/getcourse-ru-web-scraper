556af959c299cb9eeee3ff7864227abfbbcad94b
Andrey Pokoev <nc101ux@gmail.com>
2022-06-09 01:28:52 +0300

    fix

--- scrapers/phantom-script.js
+++ scrapers/phantom-script.js
@@ -1,17 +1,18 @@
-const cheerio = require("cheerio");
+"use strict";
+
 var system = require("system");
 var env = system.env;
 var page = require("webpage").create();
-var config = require("../config/config.json")
+var config = require("../config/config.json");
 
 phantom.addCookie({
-  'name'     : env.SCRAPPER_AUTH_COOKIE_NAME,   /* required property */
-  'value'    : env.SCRAPPER_AUTH_COOKIE_VALUE,  /* required property */
-  'domain'   : env.SCRAPPER_AUTH_COOKIE_DOMAIN,
-  'path'     : '/',                /* required property */
-  'httponly' : true,
-  'secure'   : false,
-  'expires'  : (new Date()).getTime() + (100000 * 60 * 60)   /* <-- expires in 100 hour */
+  'name': env.SCRAPPER_AUTH_COOKIE_NAME, /* required property */
+  'value': env.SCRAPPER_AUTH_COOKIE_VALUE, /* required property */
+  'domain': env.SCRAPPER_AUTH_COOKIE_DOMAIN,
+  'path': '/', /* required property */
+  'httponly': true,
+  'secure': false,
+  'expires': new Date().getTime() + 100000 * 60 * 60 /* <-- expires in 100 hour */
 });
 
 page.settings.userAgent = config.scrapper.phantomjs.webpage.settings.userAgent;
@@ -20,24 +21,24 @@ page.settings.userAgent = config.scrapper.phantomjs.webpage.settings.userAgent;
 page.viewportSize = config.scrapper.phantomjs.webpage.viewportSize;
 
 // open page
-page.open(env.URL, function(status) {
+page.open(env.URL, function (status) {
   if (status == "success") {
     // wait until all the assets are loaded
-    function checkReadyState() {
-      var readyState = page.evaluate(function() {
+    var checkReadyState = function checkReadyState() {
+      var readyState = page.evaluate(function () {
         return document.readyState;
       });
 
       if (readyState == "complete") {
-        var result = page.evaluate(function() {
+        var result = page.evaluate(function () {
           return document.documentElement.outerHTML;
         });
 
         var frameCount = page.framesCount;
         for (var i = 0; i < frameCount; ++i) {
-          page.switchToFrame(i)
-          result += page.frameContent
-          page.switchToMainFrame()
+          page.switchToFrame(i);
+          result += page.frameContent;
+          page.switchToMainFrame();
         }
 
         // exit and return HTML
@@ -46,7 +47,7 @@ page.open(env.URL, function(status) {
       } else {
         setTimeout(checkReadyState, 50);
       }
-    }
+    };
 
     checkReadyState();
   } else {
@@ -55,4 +56,3 @@ page.open(env.URL, function(status) {
     phantom.exit(1);
   }
 });
-
